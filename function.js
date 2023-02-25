f39d84509e7506ffe0af24970ca9252510039b6b
Franra <126279672+FranRaste@users.noreply.github.com>
2023-02-25 16:20:20 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+
+document.getElementById("Face").addEventListener("click",function () {
+    location.href="https://www.facebook.com/NichoLopez123/";
+})
+
+document.getElementById("Insta").addEventListener("click",function () {
+    location.href="https://www.instagram.com/NichoLopez123/";
+})
+
